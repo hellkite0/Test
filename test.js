e9ae7dfb36e37f8b90dfe04ac1be59a25efdc0ad
libra guest <hellkite0@gmail.com>
2012-10-30 04:36:20 -0400

    commit message

---
+++ test.js
@@ -0,0 +1,14 @@
+var http = require("http");
+//console.log(http);
+
+http.createServer(function(request, response)
+{
+    request.on("end", function()
+	{
+		response.writeHead(200, {
+			'Content-Type' : 'text/plain'
+		});
+
+		response.end('Hello HTTP!');
+	});
+}).listen(2348);
